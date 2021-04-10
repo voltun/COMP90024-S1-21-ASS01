5b8c428b92c6f99a165f0ca9bf13cea85d281026
Nicholas Wong <49605022+voltun@users.noreply.github.com>
2021-04-10 17:27:32 +0800

    Create test.py
    Testing twitter parsing for bigtwitter

---
+++ src/test.py
@@ -0,0 +1,21 @@
+import json
+import os
+
+line_count = 0
+parent_dir = os.path.dirname(os.path.dirname(__file__))
+
+#Init classes
+fp = os.path.join(parent_dir, "tinyTwitter.json")
+
+with open(fp,encoding="utf8") as json_file:
+    for line in json_file:
+        if line_count == 0:
+            total_rows = int(line.split(',')[0].split(':')[1])
+            print(total_rows)
+        elif line_count >= total_rows:
+            json_line = json.loads(line[:-3])
+            print(json_line)
+        else:
+            json_line = json.loads(line[:-2])
+            print(json_line)
+        line_count += 1
