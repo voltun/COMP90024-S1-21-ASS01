906f341edb7f5b26c85737172c653375cb9f29ef
Nicholas Wong <49605022+voltun@users.noreply.github.com>
2021-04-10 18:03:32 +0800

    Refactored grid_parser
    Added twitter parser to main
    Added grid_parser instance to main

--- src/grid_parser.py
+++ src/grid_parser.py
@@ -1,35 +1,39 @@
 import json
 import os
-parent_dir = os.path.dirname(os.path.dirname(__file__))
 
-os.path.join(parent_dir, 'melbGrid.json')
+class GridParser:
+    grid = []
 
-with open(os.path.join(parent_dir, 'melbGrid.json'),encoding="utf8") as json_file:
-    data_list = json.load(json_file)
+    def __init__(self, filename):
+        parent_dir = os.path.dirname(os.path.dirname(__file__))
 
-grid = []
+        fp = os.path.join(parent_dir, filename)
 
-for data in data_list['features']:
-    temp = []
-    temp.append(data["properties"]["id"])
-    temp.append(data["properties"]["xmin"])
-    temp.append(data["properties"]["xmax"])
-    temp.append(data["properties"]["ymin"])
-    temp.append(data["properties"]["ymax"])
-    grid.append(temp)
-print(grid)
+        with open(fp, encoding="utf8") as json_file:
+            data_list = json.load(json_file)
 
-def getCell(lat,long):
-    for i in range(len(grid)):
-        if lat > grid[i][1] and lat < grid[i][2] and long > grid[i][3] and long < grid[i][4]:
-            return grid[i][0]
-        if long == grid[i][4] and lat >= grid[i][1] and lat <= grid[i][2]:
-            return grid[i][0]
-    for i in range(len(grid)):
-        if lat == grid[i][2] and long >= grid[i][3] and long <= grid[i][4]:
-            return grid[i][0]
-        if lat >= grid[i][1] and lat <= grid[i][2] and long >= grid[i][3] and long <= grid[i][4]:
-            return grid[i][0]
+        for data in data_list['features']:
+            temp = []
+            temp.append(data["properties"]["id"])
+            temp.append(data["properties"]["xmin"])
+            temp.append(data["properties"]["xmax"])
+            temp.append(data["properties"]["ymin"])
+            temp.append(data["properties"]["ymax"])
+            self.grid.append(temp)
+        print(self.grid)
 
-
-print(getCell(144.85,-37.64))
+    def getCell(self, lat, long):
+        for i in range(len(self.grid)):
+            if lat > self.grid[i][1] and lat < self.grid[i][2]\
+                and long > self.grid[i][3] and long < self.grid[i][4]:
+                return self.grid[i][0]
+            if long == self.grid[i][4] and lat >= self.grid[i][1]\
+                and lat <= self.grid[i][2]:
+                return self.grid[i][0]
+        for i in range(len(self.grid)):
+            if lat == self.grid[i][2] and long >= self.grid[i][3]\
+                and long <= self.grid[i][4]:
+                return self.grid[i][0]
+            if lat >= self.grid[i][1] and lat <= self.grid[i][2]\
+                and long >= self.grid[i][3] and long <= self.grid[i][4]:
+                return self.grid[i][0]
--- src/main_1c1n.py
+++ src/main_1c1n.py
@@ -1,15 +1,37 @@
 import json
 import os
 from afinn_reader import AfinnReader
+from grid_parser import GridParser
+
+MELBGRIDFILENAME = "melbGrid.json"
+AFINNFILENAME = "AFINN.txt"
 
 def main():
     parent_dir = os.path.dirname(os.path.dirname(__file__))
+    twitter_fp = os.path.join(parent_dir, "tinyTwitter.json")
+
+    #Init var
+    line_count = 1
 
     #Init classes
-    afinn = AfinnReader(os.path.join(parent_dir, "AFINN.txt"))
+    afinn = AfinnReader(os.path.join(parent_dir, AFINNFILENAME))
+    grid = GridParser(MELBGRIDFILENAME)
 
     print(afinn.calcAFINNScore(["abanDoNs", "abandons.!?", "abandon abandon"]))
-
+    print(grid.getCell(144.85,-37.64))
+    #Start looping through all the tweet entries
+    with open(twitter_fp,encoding="utf8") as json_file:
+        for line in json_file:
+            if line_count == 1:
+                total_rows = int(line.split(',')[0].split(':')[1])
+                print(total_rows)
+            elif line_count >= total_rows:
+                json_line = json.loads(line[:-3])
+                print(json_line)
+            else:
+                json_line = json.loads(line[:-2])
+                print(json_line)
+            line_count += 1
 
 if __name__ == "__main__":
     main()
